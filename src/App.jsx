a17d3db9aa11cbda75c1e64488d3caa25bbc754d
Omega Tanvir <contact@omegatanvir.com>
2023-09-21 18:59:09 +0600

    Form Component Create create a form  also react & react router setup done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Form from './components/Form/Form'
+
+function App() {
+
+  return (
+    <>
+      <h2>New Form Data Collect</h2>
+      <Form></Form>
+    </>
+  )
+}
+
+export default App
